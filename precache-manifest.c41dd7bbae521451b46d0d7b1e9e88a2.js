fff5b40ab62c2294cc78e5fd47a8415849fb1cb3
Iosif Dobos <c16735789@mydit.ie>
2020-07-13 22:08:18 +0100

    Updates

---
+++ precache-manifest.c41dd7bbae521451b46d0d7b1e9e88a2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3addcd162f3a107cd071dea2c3b01af4",
+    "url": "/Iosif-Portfolio/index.html"
+  },
+  {
+    "revision": "ed176295dfffd228d290",
+    "url": "/Iosif-Portfolio/static/css/main.1041d9e6.chunk.css"
+  },
+  {
+    "revision": "08b05ca6169339a8981e",
+    "url": "/Iosif-Portfolio/static/js/2.3c3e037f.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/Iosif-Portfolio/static/js/2.3c3e037f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed176295dfffd228d290",
+    "url": "/Iosif-Portfolio/static/js/main.cfd26aba.chunk.js"
+  },
+  {
+    "revision": "306ad13728d7fb06f489",
+    "url": "/Iosif-Portfolio/static/js/runtime-main.e2eea88f.js"
+  },
+  {
+    "revision": "c7ff3dfec98120099589e7efdcca85fc",
+    "url": "/Iosif-Portfolio/static/media/github_image.c7ff3dfe.png"
+  }
+]);
